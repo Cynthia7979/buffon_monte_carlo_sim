eee8f437e8653b45682c49f4c151d0026b687585
Cynthia7979 <cynthiawang7979@hotmail.com>
2024-02-19 15:21:09 -0500

    feat: More realistic simulation - Removed dataframe support - ~~Removed Herobrine~~

--- buffon_monte_carlo.py
+++ buffon_monte_carlo.py
@@ -1,83 +1,96 @@
 # buffon monte carlo simulation
 # simulation of buffon's needle to estimate the value of pi
+# enhancement fork by Cynthia
 
 # imports
 import matplotlib.pyplot as plt
 import numpy as np
 import random as rd
-import pandas as pd
 
-# parameters ===========================================================================================================
-# sample size will determine the number of needles thrown
-# sig figs will determine the rounding of decimals for needles that cross the lines, keep to multiples of 10
-# model is generally more accurate with large sample size and sig_fig but at the cost of run time
+def main():
+    # parameters ===========================================================================================================
+    # Sample size: Determines the number of needles thrown
+    # Sig figs: Determines the rounding of decimals for needles that cross the lines, keep to multiples of 10
+    # Width: Width of a single strip
+    # Length: Length of a needle
+    # Range: Length and width of the field
 
-sample_size = 1000
-sig_figs = 10000
+    sample_size = 1000
+    sig_figs = 1e13
+    width = 1
+    length = 0.8
+    assert length < width, "Needle length cannot be longer than tile width"
+    board_range = width * 10
 
-# initializing plot ====================================================================================================
-fig = plt.figure(figsize=[9, 9])
+    plot_single_attempt(sample_size, sig_figs, width, length, board_range)
+    plt.show()
 
-fig.suptitle('Buffon\'s Needle Simulation with {} samples'.format(sample_size))
+def stupid_digits_count(num, ref):
+    a = str(num)
+    b = str(ref)
+    in_common = 0
+    while a and b:
+        if a[0] == '.':
+            a = a[1:]
+            b = b[1:]
+            continue
+        if a[0] == b[0]:
+            in_common += 1
+            a = a[1:]
+            b = b[1:]
+        else:
+            break
+    return in_common
 
-fig.subplots_adjust(hspace=.4)
-
-# defining the board
-
-board = fig.add_subplot(1, 1, 1)
-
-# defining the dataframe ===============================================================================================
-
-needles = pd.DataFrame({'x_point':[],'y_point':[],'x_end_point':[],'y_end_point':[],'crossed_line':[]})
-
-# generating the needles
-
-for n in range(0,sample_size):
-    # establishing the first point and the angle
-
-    x = rd.randrange(sig_figs,(4*sig_figs))/ sig_figs
-    y = rd.randrange(sig_figs, (4*sig_figs)) / sig_figs
+def generate_needle(sig_figs, width, length, board_range):
+    # establish the first point and the angle
+    x = rd.randrange(0, (board_range - length) * sig_figs) / sig_figs
+    y = rd.randrange(0, (board_range - length) * sig_figs) / sig_figs
     theta = rd.randrange(0,360*sig_figs) / sig_figs
 
-    # defining end point
+    # find end point
+    x_end = x + length * np.sin(np.deg2rad(theta))
+    y_end = y + length * np.cos(np.deg2rad(theta))
 
-    x_end = x + np.sin(np.rad2deg(theta))
-    y_end = y + np.cos(np.rad2deg(theta))
-
-    board.plot([x, x_end], [y, y_end])
-
-    # checking if it crossed a yard line
-    if abs(int(x)-int(x_end))==1:
+    # check if it crossed a yard line
+    crossed = False
+    left_yard_line = (x // width) * width
+    if (x > left_yard_line and x_end < left_yard_line) or (x == left_yard_line) or (x_end == left_yard_line):
         crossed = True
-    else:
-        crossed = False
+    right_yard_line = ((x // width) + 1) * width
+    if (x < right_yard_line and x_end > right_yard_line) or (x == right_yard_line) or (x_end == right_yard_line):
+        crossed = True
 
-    new_needle = [x,y,x_end,y_end,crossed]
-    print(new_needle)
+    new_needle = {'x': [x, x_end], 'y': [y, y_end], 'crossed': crossed}
+    return new_needle
 
-    needles.loc[n]=new_needle
 
-# saving the dataframe =================================================================================================
+# initializing plot ====================================================================================================
+def plot_single_attempt(sample_size, sig_figs, width, length, board_range):
+    fig = plt.figure(figsize=[board_range-1, board_range-1])
+    fig.suptitle('Buffon\'s Needle Simulation with {} samples'.format(sample_size))
+    fig.subplots_adjust(hspace=.4)
+    board = fig.add_subplot(1, 1, 1)
 
-needles.to_csv('buffons_needle_sim.csv')
+    # Generate needles
+    yard_lines = [[[i, i], [0, board_range]] for i in range(0, board_range, width)]
+    for yl in yard_lines:
+        board.plot(yl[0], yl[1],'0.01', linewidth=0.1)
 
-# adding yard lines ====================================================================================================
+    needles = []
+    for _ in range(0, sample_size):
+        needle = generate_needle(sig_figs, width, length, board_range)
+        print(needle)
+        needles.append(needle)
 
-yard_lines = [[[1,1],[0,5]],[[2,2],[0,5]],[[3,3],[0,5]],[[4,4],[0,5]]]
+        # plot needle
+        board.plot(needle['x'], needle['y'], color=('red' if needle['crossed'] else 'black'))
 
-for n in range(0,4):
-    board.plot(yard_lines[n][0],yard_lines[n][1],'k--')
+    # Estimate pi
+    crossed_count = len(list(filter(lambda ne : ne['crossed'], needles)))
+    pi_est = (2 * length * sample_size) / (width * crossed_count)
+    fig.text(.01, .9, f'Estimated value of pi: {pi_est}, Actual Value of pi: {np.pi}, Digits in common: {stupid_digits_count(pi_est, np.pi)}')
+    fig.text(.01, .92, f'Counted crossing needles: {crossed_count}, Diff ={pi_est - np.pi}')
 
-# estimating pi ========================================================================================================
-
-crossed_count = len(needles[needles['crossed_line']==True])
-
-pi_est = (2*sample_size)/crossed_count
-
-diff=pi_est-np.pi
-
-text = 'Estimated value of pi: {}, Actual Value of pi: {}, Diff ={}'.format(pi_est, np.pi, diff)
-fig.text(.01, .9, text)
-
-# calling the plot =====================================================================================================
-plt.show()
+if __name__ == "__main__":
+    main()
